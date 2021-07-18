2067d16791e2ca82e49e284327b8c61f75eb7e40
Sushant <sushantkumar6700@gmail.com>
2021-07-18 10:42:58 +0530

    add a check reboot condition and exiting

--- def.py
+++ def.py
@@ -2,12 +2,15 @@
 #this is a shabang line
 
 import os
-
+import sys
 def checkReboot():
     #returns true if the computer has a pending reboot.
     return os.path.exist("/run/reboot-required")
 
 def main():
     "some changes added here"
+    if checkReboot():
+        print("Pending reboot")
+        sys.exit(1)
 
 main()
