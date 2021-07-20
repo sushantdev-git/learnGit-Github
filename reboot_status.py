d9c716e27338295f82351be77dd66478c50a0e8e
Sushant <sushantkumar6700@gmail.com>
2021-07-20 13:22:38 +0530

    added a disk full check

--- reboot_status.py
+++ reboot_status.py
@@ -12,6 +12,9 @@ def main():
     if checkReboot():
         print("Pending reboot")
         sys.exit(1)
+    if diskFull():
+        print("disk full")
+        sys.exit(1)
     print("no pending reboot")
     sys.exit(0)
 
